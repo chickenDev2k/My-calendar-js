1b2a5800a27c48800088be9fe7d73c211e33cf52
Nivetha <taquanga6dp@gmail.com>
2024-09-27 10:57:36 +0900

    fix dir

---  calenda/main.js
+++  calenda/main.js
@@ -2,6 +2,15 @@ const yearNameEl = document.querySelector("#year-name");
 const monthNameEl = document.querySelector("#month-name");
 const dayNameEl = document.querySelector("#day-name");
 const dayNumEl = document.querySelector("#day-number");
+const backEl = document.querySelector(".back");
+const nextEl = document.querySelector(".next");
+
+const back = (date) => {
+    date.getDate - 1;
+};
+
+backEl.addEventListener("click", back);
+nextEl.addEventListener("click", back);
 
 const date = new Date();
 const year = date.getFullYear();
